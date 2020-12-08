da684cde221e1d3f14fe5e9ae7240c432b9d0220
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-12-08 10:02:38 -0700

    New Push

--- data.js
+++ data.js
@@ -1,182 +1,210 @@
-let entries = [
-    {type: "flowers" ,
-     details: [
-        {
+//QUIZ: Flowers - Total: 6 questions
+let flower1 =     {
         
-        image: "daffodil.png",
-        question: "What is the name of this flower?",
-        answer: 'Lily',
-        guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
-        "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
+    image: "daffodil.png",
+    question: "What is the name of this flower?",
+    answer: 'Lily',
+    guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
+    "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
 
-        },
-        {
-        
-            image: "sunflower.png",
-            question: "What is the name of this flower?",
-            answer: 'Sunflower',
-            guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
-            "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
-    
-        },
-        {
-        
-            image: "rose.png",
-            question: "What is the name of this flower?",
-            answer: 'Rose',
-            guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
-            "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
-    
-        },
-        {
-        
-            image: "cherryblossom.png",
-            question: "What is the name of this flower?",
-            answer: 'Cherryblossom',
-            guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
-            "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
-    
-        },
-        {
-        
-            image: "tulip.png",
-            question: "What is the name of this flower?",
-            answer: 'Tulip',
-            guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
-            "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
-    
-        },
-        {
-        
-            image: "waterlily.png",
-            question: "What is the name of this flower?",
-            answer: 'Waterlily',
-            guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
-            "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
-    
-        },
-        ]  
-    },
-    {type: "animals" ,
-     details: [
-        {
-        
-        image: "lion.jpg",
-        question: "What is the name of this animal?",
-        answer: 'Lion',
-        guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
-        "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
+    }
 
-        },
-        {
+let flower2 = {
         
-            image: "elephant.jpg",
-            question: "What is the name of this animal?",
-            answer: 'Elephant',
-            guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
-            "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
-    
-        },
-        {
-        
-            image: "camel.jpg",
-            question: "What is the name of this animal?",
-            answer: 'Camel',
-            guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
-            "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
-    
-        },
-        {
-        
-            image: "zebra.jpg",
-            question: "What is the name of this animal?",
-            answer: 'Zebra',
-            guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
-            "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
-    
-        },
-        {
-        
-            image: "gorilla.jpg",
-            question: "What is the name of this animal?",
-            answer: 'Gorilla',
-            guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
-            "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
-    
-        },
-        {
-        
-            image: "shark.jpg",
-            question: "What is the name of this animal?",
-            answer: 'Shark',
-            guesses:["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
-            "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
-    
-        },
-        ]  
-    },  
-     {type: "mathematics" ,
-        details: [
-       {
-       
-       image: "math1.jpg",
-       question: "What is the result?",
-       answer: '18',
-       guesses: ["43", "92", "-4","-43", "787", "43", "64",
-       "-1223", "1886", "98", "99", "30"]
+    image: "sunflower.png",
+    question: "What is the name of this flower?",
+    answer: 'Sunflower',
+    guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
+    "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
 
-       },
-       {
+}
+
+let flower3 =   {
+        
+    image: "rose.png",
+    question: "What is the name of this flower?",
+    answer: 'Rose',
+    guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
+    "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
+
+}
+
+let flower4 =  {
+        
+    image: "cherryblossom.png",
+    question: "What is the name of this flower?",
+    answer: 'Cherryblossom',
+    guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
+    "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
+
+}
+
+let flower5 = {
+        
+    image: "tulip.png",
+    question: "What is the name of this flower?",
+    answer: 'Tulip',
+    guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
+    "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
+
+}
+
+let flower6 =   {
+        
+    image: "waterlily.png",
+    question: "What is the name of this flower?",
+    answer: 'Waterlily',
+    guesses: ["Tulip", "Crocus", "Buttercup","Lavender" , "Carnation", "Dahlia", "Orchids",
+    "Aster", "Peony", "Hollyhock", "Foxgloves", "Hyacith"]
+
+}
+//QUIZ: Animals - Total: 6 questions
+let animal1 =  {
+        
+    image: "lion.jpg",
+    question: "What is the name of this animal?",
+    answer: 'Lion',
+    guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
+    "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
+
+    }
+let animal2 =  {
+        
+    image: "elephant.jpg",
+    question: "What is the name of this animal?",
+    answer: 'Elephant',
+    guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
+    "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
+
+}
+let animal3 =   {
+        
+    image: "camel.jpg",
+    question: "What is the name of this animal?",
+    answer: 'Camel',
+    guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
+    "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
+
+}
+let animal4 =    {
+        
+    image: "zebra.jpg",
+    question: "What is the name of this animal?",
+    answer: 'Zebra',
+    guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
+    "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
+
+}
+let animal5 =  {
+        
+    image: "gorilla.jpg",
+    question: "What is the name of this animal?",
+    answer: 'Gorilla',
+    guesses: ["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
+    "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
+
+}
+let animal6 = {
+        
+    image: "shark.jpg",
+    question: "What is the name of this animal?",
+    answer: 'Shark',
+    guesses:["Bats", "Horse", "Giraffe","Coyote", "Goose", "Leopard", "Dog",
+    "Crow", "Flamingo", "Eagle", "Tiger", "Rabbit"]
+
+}
+//QUIZ: Mathematics - Total: 6 questions
+let math1 = {
        
-           image: "math2.jpg",
-           question: "What is the result?",
-           answer: '464',
-           guesses: ["43", "92", "-4","-43", "787", "43", "64",
-           "-1223", "1886", "98", "99", "30"]
-   
-       },
-       {
+    image: "math1.jpg",
+    question: "What is the result?",
+    answer: '18',
+    guesses: ["43", "92", "-4","-43", "787", "43", "64",
+    "-1223", "1886", "98", "99", "30"]
+
+    }
+let math2 = {
+    
+    image: "math2.jpg",
+    question: "What is the result?",
+    answer: '464',
+    guesses: ["43", "92", "-4","-43", "787", "43", "64",
+    "-1223", "1886", "98", "99", "30"]
+
+}
+let math3 = {
        
-           image: "math3.jpg",
-           question: "What is the result?",
-           answer: 'undefined',
-           guesses: ["43", "92", "-4","-43", "787", "43", "64",
-           "-1223", "1886", "98", "99", "30"]
-   
-       },
-       {
+    image: "math3.jpg",
+    question: "What is the result?",
+    answer: 'undefined',
+    guesses: ["43", "92", "-4","-43", "787", "43", "64",
+    "-1223", "1886", "98", "99", "30"]
+
+}
+let math4 = {
        
-           image: "math4.jpg",
-           question: "What is the result?",
-           answer: '7',
-           guesses: ["43", "92", "-4","-43", "787", "43", "64",
-           "-1223", "1886", "98", "99", "30"]
-   
-       },
-       {
+    image: "math4.jpg",
+    question: "What is the result?",
+    answer: '7',
+    guesses: ["43", "92", "-4","-43", "787", "43", "64",
+    "-1223", "1886", "98", "99", "30"]
+
+}
+let math5 ={
        
-           image: "math5.jpg",
-           question: "What is the result?",
-           answer: '1',
-           guesses: ["43", "92", "-4","-43", "787", "43", "64",
-           "-1223", "1886", "98", "99", "30"]
-   
-       },
-       {
-       
-           image: "math6.jpg",
-           question: "What is the result?",
-           answer: '69',
-           guesses:["43", "92", "-4","-43", "787", "43", "64",
-           "-1223", "1886", "98", "99", "30"]
-   
-       },
-       ]  
-   }
-   
-];
+    image: "math5.jpg",
+    question: "What is the result?",
+    answer: '1',
+    guesses: ["43", "92", "-4","-43", "787", "43", "64",
+    "-1223", "1886", "98", "99", "30"]
+
+}
+let math6 = {
+
+    image: "math6.jpg",
+    question: "What is the result?",
+    answer: '69',
+    guesses:["43", "92", "-4","-43", "787", "43", "64",
+    "-1223", "1886", "98", "99", "30"]
+
+}
+
+
+let flowers= {
+            name: 'quiz 1', id: 1, image: 'quiz1,png', 
+            questions:[
+                flower1,
+                flower2,
+                flower3,
+                flower4,
+                flower5,
+                flower6
+            ]
+            };
+let animals = {
+            name: 'quiz 1', id: 2, image: 'quiz1,png', 
+            questions:[
+                animal1,
+                animal2,
+                animal3,
+                animal4,
+                animal5,
+                animal6
+            ]};
+let mathematics = {
+                name: 'quiz 1', id: 3, image: 'quiz1,png', 
+                questions:[
+                    math1,
+                    math2,
+                    math3,
+                    math4,
+                    math5,
+                    math6
+                ]};
+
+let quizzes = [flowers,animals,mathematics];
 
 let scores = [];
 
 module.exports.scores = scores;
-module.exports.entries = entries;
+module.exports.quizzes = quizzes;
--- server.js
+++ server.js
@@ -1,36 +1,53 @@
+//Author: Hamzah M. Firman
+//Class : ISTA 330 - Advanced Web Design
+//Assignment: Practicum 7
+//Date  : 12/08/20
+
 const express = require('express');
-var cors = require('cors');
-let data = require('./entries');
-var bodyParser = require('body-parser');
+var cors = require('cors'); // This gives access to public (whoever wants to use this API)
+let data = require('./data');
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3002; //'process.env.PORT' - Needed for production where a host can choose 
+                                       // its own preference PORT for this API (Detail: Reads 'env' file and 
+                                       // looks for 'PORT' key to get the port number)
 
 // Middlewares
-app.use(cors());
+app.use(cors()); 
 app.use(express.json()); // Converts into JSON 
 app.use(express.urlencoded({extended: true}));
 
 
-app.get('/', (request, response)=> { // Home-page
-    response.send('Welcome to Image Quiz API');
+// METHOD: GET
+app.get('/' , (request, response) => {
+    response.json("Welcome to Image Quiz API!");
 })
-
-app.get('/quizzes' , (request, response) => { //All Quizes
-    response.json(data.entries);
+// METHOD: GET
+app.get('/quizzes' , (request, response) => {
+    //Only sending the metadata 'name', 'id', and 'image'
+    // '.map()' higher order function can simply help to 
+    // only capture the wanted data
+    let metadata = data.quizzes.map(x => {
+        return {
+            name: x.name,
+            id: x.id,
+            image:x.image
+            }
+    })
+    response.json(metadata);
 })
-
-app.get('/quiz/:type' , (request, response) => {
-let quizId = request.params.type.toLowerCase();
-let found = data.entries.find(x => x.type === quizId);
+// METHOD: GET
+app.get('/quiz/:id' , (request, response) => {
+let searchFor = request.params.id;
+let found = data.quizzes.find(x => x.id === searchFor);
 if(found) {
     response.json(found);
 }
 else{
-    response.status(404).json({error: `The type ${quizId} could not be found.`})
+    response.status(404).json({error: `The type ${searchFor} could not be found.`})
 }
 
 })
-
+// METHOD: POST
 app.post('/score', (request, response) =>{
     let score = request.body;
     data.scores.push(score);
@@ -38,5 +55,5 @@ app.post('/score', (request, response) =>{
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}!`)
-})
+    console.log(`Imagequiz API listening on port ${port}!`)
+});
